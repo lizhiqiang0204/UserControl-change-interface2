dc332d3b250c6cd5fc3eb09a3396b59b5e9e330e
unknown <18518092079@163.com>
2020-03-26 11:15:04 +0800

    first commit

---
+++ WpfApp3/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+namespace WpfApp3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+
+        private UserControl1 UserControl1 = new UserControl1();//实例化用户控件1
+        private UserControl2 UserControl2 = new UserControl2();//实例化用户控件2
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        private void ButtonClick1(object sender, RoutedEventArgs e)
+        {
+            UserContent = UserControl1;//内容呈现器绑定的UserContent赋值给用户控件1
+        }
+
+        private void ButtonClick2(object sender, RoutedEventArgs e)
+        {
+            UserContent = UserControl2;//内容呈现器绑定的UserContent赋值给用户控件2
+        }
+
+        private UserControl _content;
+        //内容呈现器绑定到UserContent
+        public UserControl UserContent
+        {
+            get { return _content; }
+            set
+            {
+                _content = value;
+                OnPropertyChanged("UserContent");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ WpfApp3/UserControl2.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+
+
+namespace WpfApp3
+{
+    /// <summary>
+    /// UserControl2.xaml 的交互逻辑
+    /// </summary>
+    public partial class UserControl2 : UserControl
+    {
+        public static event EventHandler<PropertyChangedEventArgs> StaticPropertyChanged;//静态事件处理属性更改
+
+        public static int i = 0;
+        private static string _myStr = "0";
+        public static string myStr //Label 标签内容绑定到这个myStr字符串
+        {
+            get { return _myStr; }
+            set
+            {
+                _myStr = value;
+                StaticPropertyChanged?.Invoke(null, new PropertyChangedEventArgs(nameof(myStr)));//异步更新属性
+            }
+        }
+
+        public UserControl2()
+        {
+            InitializeComponent();
+            DataContext = this;//设置绑定数据的上下文为UserControl2类
+        }
+        //累加按键处理事件
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            i++;//按一下数值累加一次
+            myStr = i.ToString();//把累加完后的数值转换成字符串赋给标签内容值（立刻更新标签内容）
+        }
+
+        private void btn_Clear_Click(object sender, RoutedEventArgs e)
+        {
+            UserControl2.i = 0;//把累加计数值清空设为0
+            UserControl2.myStr = "0";//立刻更新标签内容
+
+            UserControl1.i = 0;//同时把用户界面1里的累加值清空
+            UserControl1.myStr = "0";//立刻更新用户界面1的标签内容
+        }
+    }
+}
